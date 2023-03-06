c53fe62efacb1bfe660d43fcefd64151b62cad45
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-03-05 20:27:21 -0500

    successfully extracting living wage data from DE counties and loading to S3

--- app.py
+++ app.py
@@ -3,31 +3,67 @@ import json
 import boto3
 import pandas as pd    
 from datetime import date
-        
-# def load_results_to_s3(results):
-# 	client = boto3.client(
-# 	    's3', 
-# 	    endpoint_url='https://s3.amazonaws.com',
-# 	    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
-# 	    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-# 	)
-# 	for i, obj in enumerate(results):
-# 		if len(obj) > 0:
-# 			json_data = json.dumps(obj)
-# 			client.put_object(
-# 				Bucket=os.getenv('BUCKET_NAME'), 
-# 				Key=f'real_estate/listings/{date.today()}/result_{i}.json',
-# 				Body=json_data
-# 			)
+from io import StringIO
+from dotenv import load_dotenv
+load_dotenv()
+
+def load_df_to_s3(table_name_and_df):
+	client = boto3.client(
+	    's3', 
+	    endpoint_url='https://s3.amazonaws.com',
+	    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+	    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+	)
+	table_name = table_name_and_df['table']
+	df = table_name_and_df['df']
+	csv_buffer = StringIO()
+	df.to_csv(csv_buffer, index=False)
+	client.put_object(
+		Bucket=os.getenv('BUCKET_NAME'), 
+		Key=f'real_estate/cost_of_living/{date.today()}/{table_name}.csv',
+		Body=csv_buffer.getvalue().encode('utf-8')
+	)
+
+def extract_living_wage_table(county, url):
+	tables = pd.read_html(url)
+	living_wage = tables[0]
+	wages = living_wage['Unnamed: 0_level_0']
+	living_wage = living_wage.iloc[:, 1:]
+	living_wage = living_wage.rename(mapper={
+		'1 ADULT': '1,1', 
+		'2 ADULTS(1 WORKING)': '2,1', 
+		'2 ADULTS(BOTH WORKING)': '2,2'
+	}, axis=1)
+	result = pd.DataFrame()
+	for col in set([c[0] for c in living_wage.columns]):
+		num_adults, num_working = col.split(',')
+		df = living_wage[col]
+		df.index = [v[0] for v in wages.values]
+		df = df.melt(var_name='num_children', value_name='usd_amount', ignore_index=False)
+		df = df.reset_index()
+		df['num_adults'] = num_adults
+		df['num_working'] = num_working
+		df = df.rename(columns={'index': 'wage_level'})
+		df['wage_level'] = df['wage_level'].apply(lambda x: x[:x.index('Wage')].upper())
+		df['usd_amount'] = df['usd_amount'].apply(lambda x: x[x.index('$')+1:])
+		df['num_children'] = df['num_children'].apply(lambda x: x[:x.index('Child')])
+		result = pd.concat([result, df])
+	return result
 
 def main():
-    counties_urls = {
-        'kent_county': 'https://livingwage.mit.edu/counties/10001',
-        'new_castle_county': 'https://livingwage.mit.edu/counties/10003',
-        'sussex_county': 'https://livingwage.mit.edu/counties/10005'
-    }
-    for k, v in counties_urls.items():
-	    pass
+	counties_urls = {
+		'kent_county': 'https://livingwage.mit.edu/counties/10001',
+		'new_castle_county': 'https://livingwage.mit.edu/counties/10003',
+		'sussex_county': 'https://livingwage.mit.edu/counties/10005'
+	}
+	all_tables = []
+	for k, v in counties_urls.items():
+		all_tables.append({
+			'table': f'{k}_living_wage',
+			'df': extract_living_wage_table(k, v)
+		})
+	for obj in all_tables:
+		load_df_to_s3(obj)
 
 
 if __name__ == '__main__':
